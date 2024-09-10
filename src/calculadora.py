7a3f95048be9f28aa879ce4809c6c9a22a52ad25
Adriano Aparecido da Silva <adriano.prj.ads.cloud@gmail.com>
2024-09-10 18:25:51 -0300

    feat:Adição de novas Funcionalidades: Subtração,Multiplicação e Divisão

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,20 +1,32 @@
 import oper_soma
-
+import oper_divisao
+import oper_multiplicacao
+import oper_subtracao
 
 while True:
-    operacao = input("Escolha a Operação (1-Soma ): ")
+    operacao = input("Escolha a Operação (1-Soma  2-Divisão  3-Multiplicação  4-Subtração): ")
 
-    if operacao in ['1']:
+    if operacao in ['1', '2', '3', '4']:
         num1 = float(input("Informe o primeiro número: "))
         num2 = float(input("Informe o segundo número: "))
 
         if operacao == '1':
-            print(f"Resultado: {num1} + {num2} = {oper_soma.soma(num1, num2)}")    
-            continuar = input("Deseja fazer outra operação? (s/n): ").strip().lower()
+            print(f"Resultado: {num1} + {num2} = {oper_soma.soma(num1, num2)}")
+        elif operacao == '4':
+            print(f"Resultado: {num1} - {num2} = {oper_subtracao.subtracao(num1, num2)}")
+        elif operacao == '3':
+            print(f"Resultado: {num1} X {num2} = {oper_multiplicacao.multiplicacao(num1, num2)}")
+        elif operacao == '2':
+            if num2 != 0:
+                print(f"Resultado: {num1} ÷ {num2} = {oper_divisao.divisao(num1, num2)}")
+            else:
+                print("Erro: Divisão por zero não é permitida.")
+        
+        continuar = input("Deseja fazer outra operação? (s/n): ").strip().lower()
         if continuar != 's':
             break
-        else:
-          print("Selecione uma operação válida: Soma")
+    else:
+        print("Selecione uma operação válida: Soma, Divisão, Multiplicação, Subtração.")
         continuar = input("Deseja tentar novamente? (s/n): ").strip().lower()
         if continuar != 's':
-              break
+            break
---
+++ src/oper_divisao.py
@@ -0,0 +1,5 @@
+def divisao(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return f"Erro! O número {x} não pode ser dividido por 0."
